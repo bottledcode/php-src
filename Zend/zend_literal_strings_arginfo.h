a4532b2c005126aad59dd1f257ce1cd6f37011e9
Robert Landers <landers.robert@gmail.com>
2024-08-31 16:51:44 +0200

    update factory

--- Zend/zend_literal_strings_arginfo.h
+++ Zend/zend_literal_strings_arginfo.h
@@ -1,8 +1,8 @@
 /* This is a generated file, edit the .stub.php file instead.
- * Stub hash: ad4cbcf515c04987b820e97cf268787aca46042c */
+ * Stub hash: fb2eeb9560252f439935f1ffa75ba925a23d51f1 */
 
-ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(arginfo_class_LiteralString_from, 0, 1, LiteralString, 0)
-	ZEND_ARG_OBJ_INFO(0, string, LiteralString, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_class_LiteralString_from, 0, 1, IS_STRING, 0)
+	ZEND_ARG_TYPE_INFO(0, string, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_METHOD(LiteralString, from);
@@ -17,8 +17,7 @@ static zend_class_entry *register_class_LiteralString(void)
 	zend_class_entry ce, *class_entry;
 
 	INIT_CLASS_ENTRY(ce, "LiteralString", class_LiteralString_methods);
-	class_entry = zend_register_internal_class_ex(&ce, NULL);
-	class_entry->ce_flags |= ZEND_ACC_FINAL|ZEND_ACC_NO_DYNAMIC_PROPERTIES;
+	class_entry = zend_register_internal_class_with_flags(&ce, NULL, ZEND_ACC_FINAL|ZEND_ACC_NO_DYNAMIC_PROPERTIES);
 
 	return class_entry;
 }
